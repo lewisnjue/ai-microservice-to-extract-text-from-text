dfc9baa0f06ff4f2cda73c6efd877c2177ef00a1
lewis njuw <lewiskinyuanjue.ke@gmail.com>
2025-03-09 12:40:49 +0000

    intorduced templates

--- app/main.py
+++ app/main.py
@@ -1,7 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import pathlib
+import os 
 app = FastAPI()
+BASE_DIR = pathlib.Path(__file__).resolve().parent 
+
+templates = Jinja2Templates(directory=str(BASE_DIR/'templates'))
 
 app.add_middleware(
     CORSMiddleware,
@@ -10,9 +16,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-@app.get('/') # http get 
-async def home_View():
-    return {"hello":"world"}
+@app.get('/',response_class=HTMLResponse) # http get 
+async def home_View(request:Request):
+    return  templates.TemplateResponse('home.html',{"request":request})
 
 @app.post('/') # http post 
 async def home_detail_view():
